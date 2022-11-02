0b7d90c6cea677afc16ee9c07f140e1b7f03f73d
myp <myp880@gmail.com>
2022-11-02 17:17:49 +0900

    test

--- src/pages/Parkmoonyoung/Login/LoginPmy.js
+++ src/pages/Parkmoonyoung/Login/LoginPmy.js
@@ -1,6 +1,35 @@
 import React from 'react';
 import './LoginPmy.scss';
+import { Link } from 'react-router-dom';
 
 export default function LoginPmy() {
-  return <div>Login_pmy</div>;
+  return (
+    <div className="loginWrap">
+      <div className="loginArea">
+        {/* <h1 className="logo"><a href="">instagram</a></h1> */}
+        <h1 className="logo">
+          <Link to="/signup">westagram</Link>
+        </h1>
+        <div className="inputDataArea">
+          <div className="loginFormBox">
+            <input
+              className="userId"
+              type="text"
+              placeholder="전화번호, 사용자 이름 또는 이메일"
+            />
+          </div>
+          <div className="loginFormBox">
+            <input className="userPw" type="password" placeholder="비밀번호" />
+          </div>
+          <button className="btnLogin" disabled="disabled">
+            로그인
+          </button>
+        </div>
+
+        <Link to="/main" className="forgotPw">
+          비밀번호를 잊으셨나요?
+        </Link>
+      </div>
+    </div>
+  );
 }
