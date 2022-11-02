692dc2c2a09f541d55a05d6a643436bb9a15e60b
sujeong-dev <9sujeong.dev@gmail.com>
2022-11-02 17:13:03 +0900

    ADD: 초기세팅 후 브랜치 생성

--- src/pages/Kusujeong/Login/LoginKsj.js
+++ src/pages/Kusujeong/Login/LoginKsj.js
@@ -1,6 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginKsj.scss';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginKsj() {
-  return <div>Login_ksj</div>;
+  const [value, setValue] = useState({
+    id: '',
+    pw: '',
+  });
+  const [opacity, setOpacity] = useState('0.5');
+  const [isAbled, setIsAbled] = useState(false);
+
+  const navigate = useNavigate();
+  const goToNext = () => {
+    isAbled ? navigate('/main') : alert('입력하신 정보를 다시 확인해주세요');
+  };
+
+  const saveUserValue = e => {
+    const name = e.target.name;
+    setValue({ ...value, [name]: e.target.value });
+  };
+  const setAbled = () => {
+    value.id.includes('@') && value.pw.length > 5
+      ? setIsAbled(true)
+      : setIsAbled(false);
+  };
+
+  const setBtnOpacity = () => {
+    value.id.includes('@') && value.pw.length > 5
+      ? setOpacity('1')
+      : setOpacity('0.5');
+  };
+
+  return (
+    <div className="container">
+      <h1 className="title">Westagram</h1>
+      <div className="loginContainer">
+        <form action="">
+          <input
+            className="inputinfo"
+            id="inputId"
+            name="id"
+            type="text"
+            onChange={e => {
+              saveUserValue(e);
+              setAbled();
+              setBtnOpacity();
+            }}
+            placeholder="전화번호, 사용자 이름 또는 이메일"
+          />
+          <input
+            className="inputinfo"
+            id="inputPw"
+            name="pw"
+            type="password"
+            onChange={e => {
+              saveUserValue(e);
+              setAbled();
+              setBtnOpacity();
+            }}
+            placeholder="비밀번호"
+          />
+          <button
+            type="submit"
+            className="inputinfo btn"
+            id="login-btn"
+            disabled={isAbled}
+            style={{ opacity: opacity }}
+            onClick={goToNext}
+          >
+            로그인
+          </button>
+        </form>
+      </div>
+      <a className="pwFinding" href="">
+        비밀번호를 잊으셨나요?
+      </a>
+    </div>
+  );
 }
--- src/pages/Kusujeong/Main/MainKsj.js
+++ src/pages/Kusujeong/Main/MainKsj.js
@@ -2,5 +2,314 @@ import React from 'react';
 import './MainKsj.scss';
 
 export default function MainKsj() {
-  return <h1>Hello World_ksj</h1>;
+  const imgUrl = '/images/Kusujeong/IMG_8905.JPG';
+
+  return (
+    <main className="main">
+      <article className="main-left">
+        <div className="stories">
+          <ul className="story">
+            <li>
+              <img
+                className="story-profile"
+                src={imgUrl}
+                alt="스토리 프로필이미지"
+              />
+              <span className="story-id">9sssu</span>
+            </li>
+            <li>
+              <img
+                className="story-profile"
+                src={imgUrl}
+                alt="스토리 프로필이미지"
+              />
+              <span className="story-id">9sssu</span>
+            </li>
+            <li>
+              <img
+                className="story-profile"
+                src={imgUrl}
+                alt="스토리 프로필이미지"
+              />
+              <span className="story-id">9sssu</span>
+            </li>
+            <li>
+              <img
+                className="story-profile"
+                src={imgUrl}
+                alt="스토리 프로필이미지"
+              />
+              <span className="story-id">9sssu</span>
+            </li>
+            <li>
+              <img
+                className="story-profile"
+                src={imgUrl}
+                alt="스토리 프로필이미지"
+              />
+              <span className="story-id">9sssu</span>
+            </li>
+            <li>
+              <img
+                className="story-profile"
+                src={imgUrl}
+                alt="스토리 프로필이미지"
+              />
+              <span className="story-id">9sssu</span>
+            </li>
+          </ul>
+        </div>
+        {/* <!-- 피드 --> */}
+        <div className="feed">
+          <div className="feed-header">
+            <div className="feed-logo">
+              <img
+                className="feed-profile"
+                src={imgUrl}
+                alt="피드 프로필이미지"
+              />
+              9sssu
+            </div>
+            <i className="fa-solid fa-ellipsis feed-menu" />
+          </div>
+          <div className="feed-article">
+            <img className="feed-img" src={imgUrl} alt="피드 사진" />
+          </div>
+          <div className="feed-list">
+            <div className="feed-left_list">
+              <i className="fa-regular fa-heart" />
+              <i className="fa-regular fa-comment" />
+              <i className="fa-regular fa-paper-plane" />
+            </div>
+            <div className="feed-middle_list">
+              <span className="middle-dot" />
+              <span className="middle-dot" />
+              <span className="middle-dot" />
+              <span className="middle-dot" />
+              <span className="middle-dot" />
+              <span className="middle-dot" />
+            </div>
+            <div className="feed-right_list">
+              <i className="fa-regular fa-bookmark" />
+            </div>
+          </div>
+          <div className="feed-description">
+            <div className="description-like">좋아요 3,460개</div>
+            <div className="description-article">
+              <div className="article-info">
+                <div className="ariticle-id">9sssu</div>
+                <div className="article-description">귀여운 사모예드찡</div>
+              </div>
+              <div className="article-more">...더보기</div>
+            </div>
+            <div className="description-commentAll">댓글 266개 모두 보기</div>
+            <ul id="commentList">
+              <li className="description-comment">
+                <div className="article-info">
+                  <div className="ariticle-id">rntnwjd</div>
+                  <div className="article-description">어머 너무 귀엽당~~!</div>
+                </div>
+                <div className="comment-function">
+                  <button className="comment-delete">삭제</button>
+                  <i className="fa-regular fa-heart" />
+                </div>
+              </li>
+            </ul>
+          </div>
+          <div className="description-date">2일 전</div>
+          <div className="feed-footer">
+            <div className="footer-logo">
+              <div className="footer-smile">
+                <i className="fa-regular fa-face-smile" />
+              </div>
+              <div className="footer-comment">
+                <input
+                  className="comment-input"
+                  type="text"
+                  placeholder="댓글달기"
+                />
+              </div>
+            </div>
+            <div className="footer-enter">
+              <button className="comment-btn btn">게시</button>
+            </div>
+          </div>
+        </div>
+      </article>
+      <article className="main-right">
+        <div className="right-container">
+          <div className="myProfile">
+            <div className="myProfile-logo">
+              <div className="profile-img">
+                <img
+                  className="story-profile"
+                  src={imgUrl}
+                  alt="오른쪽 아티클 내 프로필사진"
+                />
+              </div>
+              <div className="profile-info">
+                <div className="profile-id">9sssu</div>
+                <div className="profile-name">구수정</div>
+              </div>
+            </div>
+            <div className="profile-change">
+              <button className="comment-btn btn">전환</button>
+            </div>
+          </div>
+          <div className="recommand">
+            <div className="recommand-title">
+              <div className="title-1">회원님을 위한 추천</div>
+              <div className="title-2">
+                <button className="recommand-btn">모두 보기</button>
+              </div>
+            </div>
+            <div className="recommend-user">
+              <div className="recoProfile">
+                <ul className="recoProfile">
+                  <li className="recoProfile-list">
+                    <div className="recoProfile-logo">
+                      <div className="recoprofile-img">
+                        <img
+                          className="reco-profile"
+                          src={imgUrl}
+                          alt="추천친구 프로필이미지"
+                        />
+                      </div>
+                      <div className="reco-profile-info">
+                        <div className="reco-profile-id">sujeong</div>
+                        <div className="reco-profile-name">
+                          9sssu 외 1명이 팔로우중입니다.
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile-follow">
+                      <button className="follow-btn btn">팔로우</button>
+                    </div>
+                  </li>
+                  <li className="recoProfile-list">
+                    <div className="recoProfile-logo">
+                      <div className="recoprofile-img">
+                        <img
+                          className="reco-profile"
+                          src={imgUrl}
+                          alt="추천친구 프로필이미지"
+                        />
+                      </div>
+                      <div className="reco-profile-info">
+                        <div className="reco-profile-id">sujeong</div>
+                        <div className="reco-profile-name">
+                          9sssu 외 1명이 팔로우중입니다.
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile-follow">
+                      <button className="follow-btn btn">팔로우</button>
+                    </div>
+                  </li>
+                  <li className="recoProfile-list">
+                    <div className="recoProfile-logo">
+                      <div className="recoprofile-img">
+                        <img
+                          className="reco-profile"
+                          src={imgUrl}
+                          alt="추천친구 프로필이미지"
+                        />
+                      </div>
+                      <div className="reco-profile-info">
+                        <div className="reco-profile-id">sujeong</div>
+                        <div className="reco-profile-name">
+                          9sssu 외 1명이 팔로우중입니다.
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile-follow">
+                      <button className="follow-btn btn">팔로우</button>
+                    </div>
+                  </li>
+                  <li className="recoProfile-list">
+                    <div className="recoProfile-logo">
+                      <div className="recoprofile-img">
+                        <img
+                          className="reco-profile"
+                          src={imgUrl}
+                          alt="추천친구 프로필이미지"
+                        />
+                      </div>
+                      <div className="reco-profile-info">
+                        <div className="reco-profile-id">sujeong</div>
+                        <div className="reco-profile-name">
+                          9sssu 외 1명이 팔로우중입니다.
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile-follow">
+                      <button className="follow-btn btn">팔로우</button>
+                    </div>
+                  </li>
+                  <li className="recoProfile-list">
+                    <div className="recoProfile-logo">
+                      <div className="recoprofile-img">
+                        <img
+                          className="reco-profile"
+                          src={imgUrl}
+                          alt="추천친구 프로필이미지"
+                        />
+                      </div>
+                      <div className="reco-profile-info">
+                        <div className="reco-profile-id">sujeong</div>
+                        <div className="reco-profile-name">
+                          9sssu 외 1명이 팔로우중입니다.
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile-follow">
+                      <button className="follow-btn btn">팔로우</button>
+                    </div>
+                  </li>
+                  <li className="recoProfile-list">
+                    <div className="recoProfile-logo">
+                      <div className="recoprofile-img">
+                        <img
+                          className="reco-profile"
+                          src={imgUrl}
+                          alt="추천친구 프로필이미지"
+                        />
+                      </div>
+                      <div className="reco-profile-info">
+                        <div className="reco-profile-id">sujeong</div>
+                        <div className="reco-profile-name">
+                          9sssu 외 1명이 팔로우중입니다.
+                        </div>
+                      </div>
+                    </div>
+                    <div className="profile-follow">
+                      <button className="follow-btn btn">팔로우</button>
+                    </div>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <footer className="footer">
+          <ul className="recommand-footer">
+            <li className="footer-list">소개</li>
+            <li className="footer-list">도움말</li>
+            <li className="footer-list">홍보센터</li>
+            <li className="footer-list">API</li>
+            <li className="footer-list">채용정보</li>
+            <li className="footer-list">개인정보처리방침</li>
+            <br />
+            <li className="footer-list">약관</li>
+            <li className="footer-list">위치</li>
+            <li className="footer-list">언어</li>
+          </ul>
+          <span className="copyright footer">
+            <i className="fa-regular fa-copyright" />
+            2022 westagram from sujeong
+          </span>
+        </footer>
+      </article>
+    </main>
+  );
 }
