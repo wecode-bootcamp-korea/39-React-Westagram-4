91aac949ea15d1cf9426dcf279987cd89e33355b
안상준 <ahnsangjun@Dave-Ahn-MacBook-Pro.local>
2022-11-02 16:58:17 +0900

    Add:First

--- src/pages/Ahnsangjun/Login/LoginAsj.js
+++ src/pages/Ahnsangjun/Login/LoginAsj.js
@@ -2,5 +2,58 @@ import React from 'react';
 import './LoginAsj.scss';
 
 export default function LoginAsj() {
-  return <div>Login_asj</div>;
+  return (
+    <>
+      {/* 부모 로그인 클래스 페이지 */}
+      <div className="login-page">
+        {/* 1번째 자식 클래스 메인 로그인 박스  */}
+        <div className="main_login_box">
+          {/* 2번째 자식 클래스 로고 박스 */}
+          <div className="logo_box">
+            {/* 3번째 자식 아이디 위스타그램 텍스트 */}
+            <p id="westagram_text">Westagram</p>
+          </div>
+          {/* 2번째 자식 클래스 로그인 박스 */}
+          <div className="login_box">
+            {/* 3번째 자식 클래스 로그인 박스 a */}
+            <div className="login_box_a" id="login_box_id">
+              <label htmlFor="signUp" />
+              {/* 4번째 자식 클래스 사인업 박스 & 아이디 사인업 아이디 */}
+              <input
+                className="signup_box"
+                id="sign_up_id"
+                type="text"
+                placeholder="전화번호,사용자 이름 또는 이메일"
+              />
+            </div>
+            {/* 3번째 자식 클래스 로그인 박스 a */}
+            <div className="login_box_a">
+              <label htmlFor="pw" />
+              {/* 4번째 자식 클래스 사인업 박스 */}
+              <input
+                className="signup_box"
+                id="sign_up_pw"
+                type="password"
+                placeholder="비밀번호"
+              />
+            </div>
+            {/* 3번째 자식 클래스 로그인 박스 a */}
+            <div className="login_box_a">
+              <label htmlFor="login_button" />
+              {/* 4번째 자식 아이디 로그인 버튼 */}
+              <button id="login_button">로그인</button>
+            </div>
+            {/* 3번재 자식 클래스 페이스북 로그인 */}
+            <div className="facebook_login">
+              <span>Facebook으로 로그인</span>
+            </div>
+          </div>
+          {/* 2번째 자식 클래스 find password */}
+          <div className="find_password">
+            <p>비밀번호를 잊으셨나요?</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/pages/Ahnsangjun/Main/MainAsj.js
+++ src/pages/Ahnsangjun/Main/MainAsj.js
@@ -2,5 +2,170 @@ import React from 'react';
 import './MainAsj.scss';
 
 export default function MainAsj() {
-  return <h1>Hello World_asj</h1>;
+  return (
+    <div className="container">
+      {/* <!-- 네비 컨테이너 --> */}
+      <div className="nav_container">
+        <div className="nav_left">
+          <i className="fab fa-instagram" />
+          <div className="bar">|</div>
+          <div className="instagram-logo">westagram</div>
+        </div>
+
+        {/* <!-- 검색바 --> */}
+        <div className="search-container">
+          <div className="search-bar">
+            <i className="fas fa-search" />
+            <input
+              className="search-bar__input"
+              type="search"
+              placeholder="검색"
+            />
+          </div>
+        </div>
+
+        <div className="nav_right">
+          <i className="far fa-compass fa-lg" />
+          <i className="far fa-heart nav__icon fa-lg">
+            <span className="heart_notification" />
+          </i>
+          <i className="far fa-user fa-lg" />
+        </div>
+      </div>
+
+      {/* <!-- 메인섹션 --> */}
+      <section className="main_section">
+        {/* <!-- 섹션 1 --> */}
+        <div className="section_1">
+          <div className="user">
+            <img className="user__id__img" src="img/mypic.jpg" />
+            <p className="user__id">dave_ahn93</p>
+          </div>
+          <img className="cat__img" src="img/moon.jpg" />
+
+          {/* <!-- 아래 박스 --> */}
+          <div className="bottombox">
+            <div className="bottombox__icons">
+              <div className="bottombox__icons__left">
+                <i className="far fa-heart fa-lg" />
+                <i className="far fa-comment fa-lg" />
+                <i className="fa-regular fa-share-from-square fa-lg" />
+              </div>
+              <div className="bottombox__icons__right">
+                <i className="far fa-bookmark fa-lg" />
+              </div>
+            </div>
+
+            {/* <!-- 좋아요 박스 --> */}
+            <div className="bottombox__likes">
+              <div className="bottombox__likes__first">
+                <img className="dave__img" src="img/mypic.jpg" />
+                <p className="bottombox__likes__second">Liked by</p>
+                <p className="bottombox__likes__id">dave_ahn93</p>
+                <p className="bottombox__likes__thrid">and</p>
+                <p className="bottombox__likes__fourth">300 others</p>
+              </div>
+              <div className="contents">
+                <div className="contents__id">dave_ahn93</div>
+                <div className="contents__content">달</div>
+              </div>
+              <div className="comments" />
+              <div className="time">1 HOUR AGO</div>
+              <ul className="comments__list" />
+
+              {/* <!-- 댓글창 --> */}
+              <div className="write">
+                <div className="inner">
+                  <i className="fa-regular fa-face-smile" />
+
+                  <form onsubmit="return false" className="comment__form">
+                    <input
+                      className="write__input"
+                      placeholder="Add a comment..."
+                    />
+
+                    <button className="post">post</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* <!-- 섹션 2 --> */}
+        <div className="section_2">
+          <div className="my__id__box">
+            <img className="my__id" src="img/mypic.jpg" />
+            <p className="my__id__name">dave_ahn93</p>
+          </div>
+
+          {/* <!-- 스토리 & 제안 박스 --> */}
+          <div className="story__box">
+            <div className="story__box__top">
+              <p className="story">story</p>
+              <p className="story__see__all">See All</p>
+            </div>
+
+            {/* <!-- 스토리 박스 --> */}
+            <div className="story__profile">
+              {/* <!-- 친구 ID 1 --> */}
+              <div className="flower__box">
+                <div className="flower__border">
+                  <img className="flower" src="img/flower.jpeg" />
+                </div>
+                <div className="flower__border__info">
+                  <p className="flower__border__id">abc_efg</p>
+                  <p className="flower__suggested__for__you">
+                    suggested for you
+                  </p>
+                </div>
+                <div className="flower__follow">Follow</div>
+              </div>
+
+              {/* <!-- 친구 ID 2 --> */}
+              <div className="macaron__box">
+                <div className="macaron__border">
+                  <img className="macaron" src="img/macaron.jpeg" />
+                </div>
+                <div className="macaron__border__info">
+                  <p className="macaron__border__id">hij_klm</p>
+                  <p className="macaron__suggested__for__you">
+                    suggested for you
+                  </p>
+                </div>
+                <div className="macaron__follow">Follow</div>
+              </div>
+
+              {/* <!-- 친구 ID 3 --> */}
+              <div className="puppy__box">
+                <div className="puppy__border">
+                  <img className="puppy" src="img/puppy.jpeg" />
+                </div>
+                <div className="puppy__border__info">
+                  <p className="puppy__border__id">opq_rhgi</p>
+                  <p className="puppy__suggested__for__you">
+                    suggested for you
+                  </p>
+                </div>
+                <div className="puppy__follow">Follow</div>
+              </div>
+            </div>
+          </div>
+
+          {/* <!-- 제안 박스 --> */}
+          <div className="suggestions__box">
+            <div className="suggestions__box__top">
+              <p className="for__you">suggestions For You</p>
+              <p className="suggestions__see__all">See All</p>
+            </div>
+          </div>
+
+          {/* <!-- 회사 설명 --> */}
+          <p className="info">About·Help·Press·API·Jobs·Privacy·Terms·</p>
+          <p className="info2">Location·Language</p>
+          <p className="copyright">ⓒ 2022 INSTAGRAM FROM META</p>
+        </div>
+      </section>
+    </div>
+  );
 }
