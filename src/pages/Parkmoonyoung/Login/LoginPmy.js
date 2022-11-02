aa7eb80b211dfdfdf4b72b5bbd20dbc50b1899e0
myp <myp880@gmail.com>
2022-11-02 17:59:30 +0900

    Login first Update

--- src/pages/Parkmoonyoung/Login/LoginPmy.js
+++ src/pages/Parkmoonyoung/Login/LoginPmy.js
@@ -4,30 +4,30 @@ import { Link } from 'react-router-dom';
 
 export default function LoginPmy() {
   return (
-    <div className="loginWrap">
-      <div className="loginArea">
+    <div className="login-wrap">
+      <div className="login-area">
         {/* <h1 className="logo"><a href="">instagram</a></h1> */}
         <h1 className="logo">
           <Link to="/signup">westagram</Link>
         </h1>
-        <div className="inputDataArea">
-          <div className="loginFormBox">
+        <div className="input-data-area">
+          <div className="login-form-box">
             <input
-              className="userId"
+              className="user-id"
               type="text"
               placeholder="전화번호, 사용자 이름 또는 이메일"
             />
           </div>
-          <div className="loginFormBox">
+          <div className="login-form-box">
             <input className="userPw" type="password" placeholder="비밀번호" />
           </div>
-          <button className="btnLogin" disabled="disabled">
+          <button className="btn-login" disabled="disabled">
             로그인
           </button>
         </div>
 
-        <Link to="/main" className="forgotPw">
-          비밀번호를 잊으셨나요?
+        <Link to="/main" className="forgot-pw">
+          비밀번호를 잊으셨나요??
         </Link>
       </div>
     </div>
