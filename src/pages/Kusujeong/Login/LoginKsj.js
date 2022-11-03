de12630c30a8f90aebc63f6f21dcea0f56fd6cde
sujeong-dev <9sujeong.dev@gmail.com>
2022-11-03 13:59:33 +0900

    Add : Mission 2 - 로그인 버튼 활성화 기능 구현

--- src/pages/Kusujeong/Login/LoginKsj.js
+++ src/pages/Kusujeong/Login/LoginKsj.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './LoginKsj.scss';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
+import MainKsj from '../Main/MainKsj';
 
 export default function LoginKsj() {
   const [value, setValue] = useState({
@@ -11,25 +12,30 @@ export default function LoginKsj() {
   const [opacity, setOpacity] = useState('0.5');
   const [isAbled, setIsAbled] = useState(false);
 
-  const navigate = useNavigate();
-  const goToNext = () => {
-    isAbled ? navigate('/main') : alert('입력하신 정보를 다시 확인해주세요');
-  };
-
   const saveUserValue = e => {
     const name = e.target.name;
-    setValue({ ...value, [name]: e.target.value });
+    const targetValues = { ...value, [name]: e.target.value };
+    setValue(targetValues);
+    setBtnAbled(targetValues);
+    setBtnOpacity(targetValues);
+    // goToNext(targetValues);
   };
-  const setAbled = () => {
-    value.id.includes('@') && value.pw.length > 5
-      ? setIsAbled(true)
-      : setIsAbled(false);
+  // 렌더링이 한 싸이클이 돌고 나서 저장된 value를 가져오기때문에 저장된 value.id, value.pw를 쓰지 않고 saveUserValue에서 e.target.value 값들을 파라미터로 받아서 바로 사용한다.
+  const isValidated = value => {
+    return value.id.includes('@') && value.pw.length > 5 ? true : false;
+  };
+  const setBtnAbled = value => {
+    isValidated(value) ? setIsAbled(true) : setIsAbled(false);
+  };
+  const setBtnOpacity = value => {
+    isValidated(value) ? setOpacity('1') : setOpacity('0.5');
   };
 
-  const setBtnOpacity = () => {
-    value.id.includes('@') && value.pw.length > 5
-      ? setOpacity('1')
-      : setOpacity('0.5');
+  const navigate = useNavigate();
+  const goToNext = value => {
+    isValidated(value)
+      ? navigate('../Main/MainKsj')
+      : alert('입력하신 정보를 다시 확인해주세요');
   };
 
   return (
@@ -44,8 +50,6 @@ export default function LoginKsj() {
             type="text"
             onChange={e => {
               saveUserValue(e);
-              setAbled();
-              setBtnOpacity();
             }}
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
@@ -56,8 +60,6 @@ export default function LoginKsj() {
             type="password"
             onChange={e => {
               saveUserValue(e);
-              setAbled();
-              setBtnOpacity();
             }}
             placeholder="비밀번호"
           />
