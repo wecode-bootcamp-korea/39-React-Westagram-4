6db66f6c1d971eef89356c0013674e83f1f8e1f6
myp <myp880@gmail.com>
2022-11-03 14:00:20 +0900

    component practice

--- src/pages/Parkmoonyoung/Login/LoginPmy.js
+++ src/pages/Parkmoonyoung/Login/LoginPmy.js
@@ -7,29 +7,42 @@ export default function LoginPmy() {
     <div className="loginWrap">
       <div className="loginArea">
         {/* <h1 className="logo"><a href="">instagram</a></h1> */}
-        <h1 className="logo">
-          <Link to="/signup">westagram</Link>
-        </h1>
-        <div className="inputDataArea">
-          <div className="loginFormBox">
-            <input
-              className="userId"
-              type="text"
-              placeholder="전화번호, 사용자 이름 또는 이메일"
-            />
-          </div>
-          <div className="loginFormBox">
-            <input className="userPw" type="password" placeholder="비밀번호" />
-          </div>
-          <button className="btnLogin" disabled="disabled">
-            로그인
-          </button>
-        </div>
+        <HeadingTag1 />
+
+        <InputDataArea />
 
         <Link to="/main" className="forgotPw">
           비밀번호를 잊으셨나요?
         </Link>
       </div>
+    </div>
+  );
+}
+
+function HeadingTag1() {
+  return (
+    <h1 className="logo">
+      <Link to="/signup">westagram</Link>
+    </h1>
+  );
+}
+
+function InputDataArea() {
+  return (
+    <div className="inputDataArea">
+      <div className="loginFormBox">
+        <input
+          className="userId"
+          type="text"
+          placeholder="전화번호, 사용자 이름 또는 이메일"
+        />
+      </div>
+      <div className="loginFormBox">
+        <input className="userPw" type="password" placeholder="비밀번호" />
+      </div>
+      <button className="btnLogin" disabled="disabled">
+        로그인
+      </button>
     </div>
   );
 }
