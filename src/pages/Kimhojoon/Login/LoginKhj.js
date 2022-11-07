d6a7faf891d4a0946dd189b6a410d072ea18372e
HOJOON07 <ghwns107@naver.com>
2022-11-08 01:41:03 +0900

    ADD:SCSS변환

--- src/pages/Kimhojoon/Login/LoginKhj.js
+++ src/pages/Kimhojoon/Login/LoginKhj.js
@@ -3,25 +3,29 @@ import './LoginKhj.scss';
 
 export default function LoginKhj() {
   return (
-    <div class="container">
-      <div class="westagram">Westagram</div>
-      <div class="userinfo">
-        <div class="id">
+    <div className="container">
+      <div className="westagram">Westagram</div>
+      <div className="userinfo">
+        <div className="id2">
           <input
-            class="inputid"
+            className="inputid"
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
           />
         </div>
-        <div class="password">
-          <input class="inputpassword" type="password" placeholder="비밀번호" />
+        <div className="password">
+          <input
+            className="inputpassword"
+            type="password"
+            placeholder="비밀번호"
+          />
         </div>
       </div>
 
-      <button class="loginbutton" type="button" disabled>
+      <button className="loginbutton" type="button" disabled>
         로그인
       </button>
-      <div class="forget">
+      <div className="forget">
         <a href="https://www.instagram.com/accounts/password/reset/">
           비밀번호를 잊으셨나요?
         </a>
