d3249a289f0b4ae18a2aa40f07b5593ade53add4
myp <myp880@gmail.com>
2022-11-08 21:45:57 +0900

    피드  중간업로드 2

--- src/pages/Parkmoonyoung/Main/MainPmy.js
+++ src/pages/Parkmoonyoung/Main/MainPmy.js
@@ -105,12 +105,9 @@ export default function MainPmy() {
                   <div className="feedsHeader">
                     <div className="profileBox">
                       <div className="imgBox">
-                        <img
-                          src="/images/Parkmoonyoung/profile/my-profile.jpg"
-                          alt="myp"
-                        />
+                        <img src={feeds.userThumbImgUrl} alt="myp" />
                       </div>
-                      <span className="nickName">myp</span>
+                      <span className="nickName">{feeds.userId}</span>
                     </div>
                     <button className="btn detail">
                       <span className="blind">상세보기</span>
@@ -121,10 +118,7 @@ export default function MainPmy() {
                     <div className="feedsImgArea">
                       <ul>
                         <li>
-                          <img
-                            src="/images/Parkmoonyoung/profile/my-profile.jpg"
-                            alt="myp"
-                          />
+                          <img src={feeds.userFeedsImgUrl} alt="myp" />
                         </li>
                       </ul>
                     </div>
@@ -181,88 +175,6 @@ export default function MainPmy() {
                 </div>
               );
             })}
-
-            {/*
-            <div className="feedsArea">
-              <div className="feedsHeader">
-                <div className="profileBox">
-                  <div className="imgBox">
-                    <img
-                      src="/images/Parkmoonyoung/profile/my-profile.jpg"
-                      alt="myp"
-                    />
-                  </div>
-                  <span className="nickName">myp</span>
-                </div>
-                <button className="btn detail">
-                  <span className="blind">상세보기</span>
-                </button>
-              </div>
-
-              <div className="feedsContentsArea">
-                <div className="feedsImgArea">
-                  <ul>
-                    <li>
-                      <img
-                        src="/images/Parkmoonyoung/profile/my-profile.jpg"
-                        alt="myp"
-                      />
-                    </li>
-                  </ul>
-                </div>
-                <div className="feedsUtilArea">
-                  <div className="feedsBtnArea">
-                    <button className="btn heart active">
-                      <span className="blind">좋아요</span>
-                    </button>{' '}
-                    <button className="btn reple">
-                      <span className="blind">리플쓰기</span>
-                    </button>
-                    <button className="btn share">
-                      <span className="blind">공유하기</span>
-                    </button>
-                    <button className="btn favorite">
-                      <span className="blind">즐겨찾기</span>
-                    </button>
-                  </div>
-                  <div className="repleTotal">
-                    <div className="imgBox">
-                      <img
-                        src="/images/Parkmoonyoung/profile/my-profile.jpg"
-                        alt="myp"
-                      />
-                    </div>
-                    <p className="totalTxt">
-                      <span className="firstlyNick">aineworld</span>님{' '}
-                      <span className="totalNumber">외 10명</span>이 좋아합니다
-                    </p>
-                  </div>
-                  <div className="myReple reduce">
-                    <b className="myName">myp</b>
-                    <span className="txt">
-                      위워크에서 진행한 베이킹 클래스...
-                    </span>
-                    <button className="btn more">더 보기</button>
-                  </div>
-                  <ul className="repleList">
-                    {renderReple}
-                  </ul>
-                </div>
-                <div className="addRepleBox">
-                  <input
-                    type="text"
-                    className="replePost"
-                    placeholder="댓글 달기..."
-                    onChange={valueCheck}
-                    value={isRepleValue}
-                  />
-                  <button className="btn post" onClick={addPost}>
-                    게시
-                  </button>
-                </div>
-              </div>
-            </div>
-            */}
           </div>
 
           <div className="feedsAsideArea">
