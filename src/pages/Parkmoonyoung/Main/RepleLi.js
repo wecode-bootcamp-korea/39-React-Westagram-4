1079a5d5f6afc32c7b45f7b391806d09dbd712fd
myp <myp880@gmail.com>
2022-11-08 10:24:40 +0900

    리플삭제기능 추가

--- src/pages/Parkmoonyoung/Main/RepleLi.js
+++ src/pages/Parkmoonyoung/Main/RepleLi.js
@@ -1,15 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function RepleLi(props) {
+  const [isLiStatus, setLiStatus] = useState('');
+  function liHidden() {
+    setLiStatus('hidden');
+  }
+
   return (
-    <li>
+    <li className={isLiStatus}>
       <b className="nickName">neceosecius</b>
       <span className="txt">{props.data.text}</span>
       {/* .on 클래스 추가시 활성화 */}
       <button className="btn heart">
         <span className="blind">좋아요</span>
       </button>
-      <button className="btn delete">
+      <button className="btn delete" onClick={liHidden}>
         <span className="blind">삭제</span>
       </button>
       <p className="prevTime">42분 전</p>
