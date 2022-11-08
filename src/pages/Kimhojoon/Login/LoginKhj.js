57722b7810c03031d970f87cd4c936a3f1e524f9
HOJOON07 <ghwns107@naver.com>
2022-11-08 15:03:33 +0900

    ADD:로그인 버튼 활성화

--- src/pages/Kimhojoon/Login/LoginKhj.js
+++ src/pages/Kimhojoon/Login/LoginKhj.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginKhj.scss';
+import { useNavigate } from 'react-router-dom';
+import { isDisabled } from '@testing-library/user-event/dist/utils';
 
 export default function LoginKhj() {
+  const navigate = useNavigate();
+  const goToMain = () => {
+    navigate('/MainKhj');
+  };
+
+  const [idValue, setIdValue] = useState('');
+  const saveUserId = e => {
+    setIdValue(e.target.value);
+  };
+
+  const [pwValue, setPwValue] = useState('');
+  const saveUserPw = e => {
+    setPwValue(e.target.value);
+  };
+
+  // const enter = e => {
+  //   if (e.code == 'Enter') {
+  //     goToMain();
+  //   }
+  // };
+
   return (
     <div className="container">
       <div className="westagram">Westagram</div>
@@ -11,6 +34,7 @@ export default function LoginKhj() {
             className="inputid"
             type="text"
             placeholder="전화번호, 사용자 이름 또는 이메일"
+            onChange={saveUserId}
           />
         </div>
         <div className="password">
@@ -18,11 +42,18 @@ export default function LoginKhj() {
             className="inputpassword"
             type="password"
             placeholder="비밀번호"
+            onChange={saveUserPw}
+            // onKeyUp={enter} 활성화가 안되어잇어도 엔터누르면 넘어감
           />
         </div>
       </div>
 
-      <button className="loginbutton" type="button" disabled>
+      <button
+        className="loginbutton"
+        onClick={goToMain}
+        type="button"
+        disabled={idValue.includes('@') && pwValue.length >= 5 ? false : true}
+      >
         로그인
       </button>
       <div className="forget">
